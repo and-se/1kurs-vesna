47c7f5c7ad72a5f4c7ad4153db7c45fd5f5a6c46
Korolev_petr <123>
2018-03-14 11:05:52 +0300

    Done task1

--- Task1/task1/main.cpp
+++ Task1/task1/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+void setPointer (int a){
+    int* pa=&a;
+    cout<<"адрес копии переменной k: "<<pa<<endl;
+    cout<<"a(копия k)= "<<a<<endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(0, "");
+    int k(9);
+    int* pk=&k;
+    cout<<"адрес переменной k: "<<pk<<endl;
+    setPointer(k);// в переменную a копируется переменная k
+    cout<<"k= "<<k<<endl;
     return 0;
 }
