a86e6b80c058c6290ac4d0931bbbf1e3ddbb2ef8
Fedor Zolotukhin <bat312@yandex.ru>
2018-03-14 10:43:29 +0300

    Fixed output in task 1

--- Task1/discovery.cpp
+++ Task1/discovery.cpp
@@ -6,28 +6,28 @@ int* getPointer (int value);
 void ShowBin (int a);
 
 int main (int argc, char *argv[]) {
-    std::cout << "Задание 1" << std::endl;
+    std::cout << "Task 1" << std::endl;
     int k = 9;
     int* pk = &k;
-    std::cout << "Адрес переменной: " << std::endl;
+    std::cout << "Adress of variable: " << std::endl;
     std::cout << pk << std::endl; // Вывод адреса переменной
     setValue(k,10); // Передача переменной в качестве аргумента
 
-    std::cout << "Задание 2" << std::endl;
-    std::cout << "Значение адреса в памяти после первого вызова:" << std::endl;
+    std::cout << "Task2" << std::endl;
+    std::cout << "Value of adress in memory after first fuction call:" << std::endl;
     int* p_getPointer = getPointer(0);
     std::cout << *p_getPointer << std::endl; // Вывод значения по сохранённому адресу
-    std::cout << "Значение того же адреса после поторного вызова:" << std::endl;
+    std::cout << "Value of the same adress after second function call:" << std::endl;
     getPointer(10); // Вызов функции с другим аргументом
-    std::cout << *p_getPointer << std::endl; // Повторный вывод значения по тому же аресу
+    std::cout << *p_getPointer << std::endl << std::endl; // Повторный вывод значения по тому же аресу
 
-    std::cout << "Доп. задание 2" << std::endl;
-    std::cout << "Размер типа Int " << sizeof(int) << " байт" << std::endl;
+    std::cout << "Additional task 2" << std::endl;
+    std::cout << "Size of Int type " << sizeof(int) << " byte" << std::endl;
     int positiveNum = 1023;
     int negativeNum = -1023;
-    std::cout << "Число 1023 в памяти:" << std::endl;
+    std::cout << "Num 1023 in memory:" << std::endl;
     ShowBin(positiveNum);
-    std::cout << "Число -1023 в памяти:" << std::endl;
+    std::cout << "Num -1023 in memory:" << std::endl;
     ShowBin(negativeNum);
     // Отрицательные числа в дополненном коде, это значит, что знак кодируется первым битом
 
@@ -37,8 +37,8 @@ int main (int argc, char *argv[]) {
 
 void setValue (int v, int value) {
     int* pv = &v;
-    std::cout << "Адрес аргумента в памяти:" << std::endl;
-    std::cout << pv << std::endl; // Вывод адреса аргумента
+    std::cout << "Adress of argument in memory:" << std::endl;
+    std::cout << pv << std::endl << std::endl; // Вывод адреса аргумента
     v = value;
 }
 
@@ -49,17 +49,17 @@ int* getPointer (int value) {
     return pa; // Возвращение адреса локальной переменой в качестве результата
 }
 
-void ShowBin (int a) { 
-    
+void ShowBin (int a) {
+
     for (int i = 0; i < 32; i++) {
-        
+
         //Проверяем старший бит
         if (a & 2147483648) {
             std::cout << "1";
         } else {
             std::cout << "0";
         }
-        
+
         //Сдвигаем влево на 1 бит
         a = a << 1;
     }
