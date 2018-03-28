388efcfbd8e77936a779a6aff516a880a9c78a2f
колян <133>
2018-03-28 10:06:15 +0300

    это прогресс задания 2

---
+++ Task2 - модули/ZagolovokIntArr/IntArrHeader.h
@@ -0,0 +1,28 @@
+// Ёти вещи нам пока не нужны
+#ifndef INTARR_H_INCLUDED
+#define INTARR_H_INCLUDED
+
+/// “ип данных массива
+struct IntArray
+{
+    int* data;
+    int* size;
+};
+
+/// —оздаЄт массив длинны size и возвращает его
+IntArray CreateArray(int size);
+
+/// ”дал€ет переданный массив (освобождает пам€ть)
+void DeleteArray(IntArray array);
+
+/*¬ переменную result возвращаетс€ значение элемента с индексом index массива array
+¬озвращает false при выходе за границу массива*/
+bool Get (IntArray array, int index, int& result);
+
+/*Ёлементу с индексом index array присваиваетс€ значение value
+¬озвращает false при выходе за границу массива*/
+bool Set (IntArray array, int index, int value);
+
+
+#endif // INTARR_H_INCLUDED
+
